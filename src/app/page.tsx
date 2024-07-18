74541f998bf3987724de81ce698efc9a8e2bc9c3
KewinBarboza <kewinbarboza@gmail.com>
2024-07-18 19:45:20 -0400

    update prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,16 +49,14 @@ export default function Home() {
         method: 'POST',
         body: JSON.stringify({
           prompt: `Soy un desarrollador ${values.experiencia} de ${values.pais} y voy a trabajar en un proyecto para la
-          creación de ${values.tipo_proyecto} el tiempo seria ${values.tiempo} con el lenguajes ${values.lenguaje.join(',')} y con
-          el framework ${values.framework.join(',')} mi nivel educativo es ${values.nivel_educativo} soy un desarrollador ${values.desarrollador} y mis conocimiento
-          sobre base de datos son de nivel ${values.conocimiento_bd} cuando debería cobrar`,
+creación de ${values.tipo_proyecto} el tiempo seria ${values.tiempo} con el lenguajes ${values.lenguaje.join(',')} y con
+el framework ${values.framework.join(',')} mi nivel educativo es ${values.nivel_educativo} soy un desarrollador ${values.desarrollador} y mis conocimiento
+sobre base de datos son de nivel ${values.conocimiento_bd} cuando debería cobrar`,
         }),
       })
 
       const response = await result.json()
       setGeneration(response.object)
-      console.log(response.object)
-
     } catch (error) {
       console.log(error)
     } finally {
