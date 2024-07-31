1d7f6afbe4f7d33480d83368c34d4a109fa4d4ad
KewinBarboza <kewinbarboza@gmail.com>
2024-07-31 19:16:54 -0400

    update sizen in movil

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,17 +4,17 @@ import { ScrollArea } from './ui/scroll-area'
 
 export function Loading() {
   return (
-    <ScrollArea className="flex flex-col space-y-3 px-4 overflow-auto min-h-[90dvh] max-h-[68dvh]">
+    <ScrollArea className="flex flex-col space-y-3 px-4 overflow-y-auto min-h-[90dvh] max-h-[68dvh]">
       <Skeleton className="h-[45px] rounded-none" />
       <div className="space-y-2">
         <Skeleton className="h-4 rounded-none" />
         <div className='flex gap-x-3'>
-          <Skeleton className="h-36 rounded-none w-[200px]" />
+          <Skeleton className="h-36 rounded-none w-[400px] md:w-[200px]" />
           <Skeleton className="h-36 rounded-none w-full" />
         </div>
         <div className='flex gap-x-3'>
           <Skeleton className="h-36 rounded-none w-full" />
-          <Skeleton className="h-36 rounded-none w-[200px]" />
+          <Skeleton className="h-36 rounded-none w-[400px] md:w-[200px]" />
         </div>
 
         <Skeleton className="h-[145px] rounded-none" />
